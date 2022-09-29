2459ef70336807b3d7ef4375828febfbaf4a6f63
Dmitriy <dmitriy.p.pinchuk@gmail.com>
2022-09-29 21:01:51 +0300

    iframe

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
-      РАБОТАЕТ
+    <div style={{ width: '700px', height: '700px' }}>
+      <iframe aria-disabled title="myFrame" data-src="https://hoodmaps.com/embed/moscow" src="https://hoodmaps.com/embed/moscow" />
     </div>
   );
 }
