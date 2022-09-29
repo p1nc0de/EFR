5aa41848c589d783e8599c96ac7212f7892fb4b7
Dmitriy <dmitriy.p.pinchuk@gmail.com>
2022-09-29 13:36:18 +0300

    init

---
+++ client/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': 0,
+    'no-restricted-exports': 0,
+    'react/prop-types': 0,
+    'no-console': 0,
+    'react/jsx-no-constructed-context-values': 0,
+    'default-param-last': 0,
+  },
+};
---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      РАБОТАЕТ
+    </div>
+  );
+}
+
+export default App;
