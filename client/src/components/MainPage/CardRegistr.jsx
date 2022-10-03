3981fff0e810e63690ed52c41d0ce900e542b803
Dmitriy <dmitriy.p.pinchuk@gmail.com>
2022-10-03 21:18:00 +0300

    cool

--- client/src/components/MainPage/CardRegistr.jsx
+++ client/src/components/MainPage/CardRegistr.jsx
@@ -8,33 +8,31 @@ import { Box, CardActions, Input } from '@mui/material';
 import { NavLink } from 'react-router-dom';
 
 export default function CardRegistr() {
+  return (
+    <Box>
+      <Card sx={{ maxWidth: 300, bgcolor: 'text.disabled' }}>
+        <CardMedia
+          style={{ padding: '5px', borderRadius: '10px' }}
+          component="img"
+          alt="Надо зарегаться"
 
-    return (
-        <Box>
-            <Card sx={{ maxWidth: 300, bgcolor: 'text.disabled' }}>
-                <CardMedia style={{ padding: '5px', borderRadius: '10px' }}
-                    component="img"
-                    alt="Надо зарегаться"
-
-                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNjEw5JZ9OZd3xQx6bd2ENSgMnACsS7xt2Rw&usqp=CAU"
-                />
-                <CardContent>
-                    {/* <Typography gutterBottom variant="h6" component="div">
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNjEw5JZ9OZd3xQx6bd2ENSgMnACsS7xt2Rw&usqp=CAU"
+        />
+        <CardContent>
+          {/* <Typography gutterBottom variant="h6" component="div">
                         Зарегистрируйся
                     </Typography> */}
-                </CardContent>
-                <CardActions>
-                    <Button component={NavLink} to='/login' variant="contained" color='error' size="sm" sx={{ textDecoration: 'none' }}>
-                        Войти
-                    </Button>
-                    <Button component={NavLink} to='/signup' variant="contained" color='error' size="sm" sx={{ textDecoration: 'none' }}>
-                        Зарегаться
-                    </Button>
-                </CardActions>
+        </CardContent>
+        <CardActions>
+          <Button component={NavLink} to="/login" variant="contained" color="error" size="sm" sx={{ textDecoration: 'none' }}>
+            Войти
+          </Button>
+          <Button component={NavLink} to="/signup" variant="contained" color="error" size="sm" sx={{ textDecoration: 'none' }}>
+            Зарегаться
+          </Button>
+        </CardActions>
 
-            </Card >
-        </Box>
-    );
+      </Card>
+    </Box>
+  );
 }
-
-
