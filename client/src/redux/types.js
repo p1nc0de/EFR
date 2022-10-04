53dd67bdaf3b9f69d21c66aeb2763b28726ae351
Tanya <9152204088zz@gmail.com>
2022-10-04 10:14:36 +0300

    1

--- client/src/redux/types.js
+++ client/src/redux/types.js
@@ -15,7 +15,7 @@ export const SET_COUNTRY_CITIES = 'SET_COUNTRY_CITIES';
 // Users
 export const SET_ALL_USERS = 'SET_USERS';
 
-export const SET_USER = 'SET_USERS';
+export const SET_USER = 'SET_USER';
 export const UPDATE_USER = 'UPDATE_USER';
 export const DELETE_USER = 'DELETE_USER';
 
