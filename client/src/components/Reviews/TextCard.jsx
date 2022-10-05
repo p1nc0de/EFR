3fecc956c5f70cd16fa0c030ff366812bd96bd1c
Tanya <9152204088zz@gmail.com>
2022-10-05 11:21:36 +0300

    flex review

--- client/src/components/Reviews/TextCard.jsx
+++ client/src/components/Reviews/TextCard.jsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import {
+  Avatar, Button, CardActions, Grid,
+} from '@mui/material';
 import { useDispatch } from 'react-redux';
-import { Col, Row } from 'reactstrap';
 import { deleteReviewAsync } from '../../redux/actions/reviewActions';
 
 export default function TextCard({ authUser, rev, id }) {
@@ -13,38 +11,40 @@ export default function TextCard({ authUser, rev, id }) {
 
   return (
 
-    <Card sx={{ maxWidth: 1000 }}>
+    <Grid items sx={{ magrinLeft: '5vw', marginTop: '-5vw' }}>
+      <div className="total-review">
+        <Avatar
+          alt="EFR"
+          src={`${rev?.User?.avatar}`}
+          sx={{ width: 54, height: 54 }}
+        />
+        <div className="right-text">
+          <div className="high">
+            <h6>
+              {' '}
+              {rev?.review}
+            </h6>
+          </div>
+          <div className="low">
+            <small>
+              {' '}
+              Дата публикации:
+              {' '}
+              {new Date(rev?.createdAt).toLocaleString()}
+              {' '}
+            </small>
+            <div className="name">
+              {' '}
+              Автор:
+              <Button sx={{ color: 'red' }}>{rev?.User?.login}</Button>
 
-      <CardActionArea>
-        <Row>
-          <Col>
-            <CardMedia
-              component="img"
-              height="240"
-              image={`${rev?.User?.avatar}`}
-              alt="фотография 10х12"
-            />
-          </Col>
-          <Col>
-            <CardContent sx={{ width: '500px' }}>
-              <Typography gutterBottom variant="body2" component="div">
-                {rev?.review}
-              </Typography>
-              <small>
-                {' '}
-                Дата публикации:
-                {' '}
-                {new Date(rev?.createdAt).toLocaleString()}
-                {' '}
-              </small>
-              <Typography variant="body2" color="text.secondary">
-                Автор:
-                <Button sx={{ color: 'red' }}>{rev?.User?.login}</Button>
-              </Typography>
-            </CardContent>
-          </Col>
-          {/* //отображение кнопок только автору ревью */}
-          { authUser.id === rev?.User?.id
+            </div>
+          </div>
+
+        </div>
+      </div>
+      {/* //отображение кнопок только автору ревью */}
+      { authUser.id === rev?.User?.id
       && (
       <CardActions>
         <Button
@@ -57,10 +57,7 @@ export default function TextCard({ authUser, rev, id }) {
         </Button>
       </CardActions>
       )}
-        </Row>
-      </CardActionArea>
-
-    </Card>
+    </Grid>
 
   );
 }
