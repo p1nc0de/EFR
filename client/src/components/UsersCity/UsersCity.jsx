5221508c93f7075fa276ffa60471d5d313a115ca
Olga Pavlova <lelya140494@gmail.com>
2022-10-05 09:59:26 +0300

    users in main style

--- client/src/components/UsersCity/UsersCity.jsx
+++ client/src/components/UsersCity/UsersCity.jsx
@@ -1,20 +1,14 @@
 import React, { useEffect } from 'react';
-import { useParams, NavLink } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  Avatar, Grid,
-  // Button,
-  // CardActions,
-  // Modal,
-  // Box,
-} from '@mui/material';
+import { Grid } from '@mui/material';
 
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import { getAllUsers } from '../../redux/actions/userActions';
+import { fetchAllCities } from '../../redux/actions/cityActions';
+import PhotoBackground from '../UI/PhotoBackground';
+import CityNavbar from '../UI/CityNavbar';
+import UsersToday from './UsersToday';
+import UsersTomorrow from './UsersTomorrow';
 
 // import UserPage from '../UserPage/UserPage';
 
@@ -32,68 +26,37 @@ import { getAllUsers } from '../../redux/actions/userActions';
 // };
 
 export default function UsersCity() {
-  const { id } = useParams();
   const dispatch = useDispatch();
+  const { id } = useParams();
+
+  useEffect(() => {
+    dispatch(fetchAllCities());
+  }, []);
+  const { city } = useSelector((s) => s);
+  const oneCity = city[id - 1];
+
   const users = useSelector((state) => state.user);
-
-  // const [open, setOpen] = React.useState(false);
-
   useEffect(() => {
     dispatch(getAllUsers(id));
   }, []);
 
+  // console.log('main', id);
+  // console.log('main', city);
+  // console.log('main', oneCity);
+  
+  // const [open, setOpen] = React.useState(false);
+
   return (
-    <>
-      <Typography variant="h2" component="div">
-        Обитатели
-      </Typography>
-      <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        spacing={3}
-      >
-
-        {users && users?.map((user) => (
-          <Grid
-            container
-            item
-            xs={2}
-            key={user.id}
-            direction="column"
-            justifyContent="center"
-            alignItems="center"
-
-          >
-            <Card sx={{ minWidth: 275 }}>
-              <CardContent justifyContent="center" alignItems="center">
-                {user?.avatar?.includes('http')
-                  ? (
-                    <Avatar
-                      alt="Remy Sharp"
-                      src={user?.avatar}
-                      sx={{ width: 170, height: 170 }}
-                    />
-                  )
-                  : (
-                    <Avatar
-                      alt="Remy Sharp"
-                      src={`http://localhost:3001/photos/${user?.avatar}`}
-                      sx={{ width: 170, height: 170 }}
-                    />
-                  )}
-              </CardContent>
-              <CardActions>
-                <Button component={NavLink} to={`/users/${user?.id}`} variant="contained" color="grey" size="sm" sx={{ textDecoration: 'none' }}>
-                  {user?.login}
-                </Button>
-
-              </CardActions>
-            </Card>
-          </Grid>
-        ))}
+    <Grid container direction="column" sx={{ color: 'text.primary' }}>
+      <Grid item>
+        <PhotoBackground oneCity={oneCity} />
       </Grid>
-    </>
+      <Grid item>
+        <CityNavbar />
+      </Grid>
+
+      <UsersToday users={users} oneCity={oneCity} />
+      <UsersTomorrow users={users} oneCity={oneCity} />
+    </Grid>
   );
 }
